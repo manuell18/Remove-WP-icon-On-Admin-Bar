a7c10b4c857b72b2c132af74ee1e24ba7ae2e8d9
manuell18 <emmmanuelmilimowams@gmail.com>
2023-05-20 16:27:13 +0300

    third commit, added remove_wp function

--- remove_wp_icon_on_admin_bar.php
+++ remove_wp_icon_on_admin_bar.php
@@ -11,24 +11,12 @@
  */
 
 
-function replace_admin_bar_logo() {
+ function remove_wp_logo() {
     global $wp_admin_bar;
 
-    // Remove the WordPress logo
-    $wp_admin_bar->remove_node('wp-logo');
-
-    // Add a custom home icon
-    $args = array(
-        'id'    => 'custom-home-icon',
-        'title' => '<span class="ab-icon"></span>',
-        'href'  => home_url(),
-        'meta'  => array(
-            'class' => 'ab-icon',
-            'title' => 'Home',
-        ),
-    );
-    $wp_admin_bar->add_node($args);
+    $wp_admin_bar->remove_menu('wp-logo');
 }
-add_action('wp_before_admin_bar_render', 'replace_admin_bar_logo', 99);
+add_action('wp_before_admin_bar_render', 'remove_wp_logo');
+
 
 ?>
